18d906bdcea3216baecaa735d29882678419397b
SourabhPatil <202251095@iiitvadodara.ac.in>
2024-08-22 10:58:36 +0530

    "Made The Code Friendly For Variable Number"

--- missionary_cannibal_bfs.py
+++ missionary_cannibal_bfs.py
@@ -1,33 +1,29 @@
 from collections import deque
 
-def is_valid(state):
+def is_valid(state,num):
     missionaries, cannibals, boat = state
-    if missionaries < 0 or cannibals < 0 or missionaries > 3 or cannibals > 3:
+    if missionaries < 0 or cannibals < 0 or missionaries > num or cannibals > num:
         return False
     if missionaries > 0 and missionaries < cannibals:
         return False
-    if 3 - missionaries > 0 and 3 - missionaries < 3 - cannibals:
+    if num - missionaries > 0 and num - missionaries < num - cannibals:
         return False
     return True
 
-def get_successors(state):
+def get_successors(state,num):
     successors = []
     missionaries, cannibals, boat = state
-    if boat == 1:
-        moves = [(2, 0), (0, 2), (1, 1), (1, 0), (0, 1)]
-        for move in moves:
+    moves = [(2, 0), (0, 2), (1, 1), (1, 0), (0, 1)]
+    for move in moves:
+        if boat == 1:
             new_state = (missionaries - move[0], cannibals - move[1], 0)
-            if is_valid(new_state):
-                successors.append(new_state)
-    else:
-        moves = [(2, 0), (0, 2), (1, 1), (1, 0), (0, 1)]
-        for move in moves:
+        else:
             new_state = (missionaries + move[0], cannibals + move[1], 1)
-            if is_valid(new_state):
-                successors.append(new_state)
+        if is_valid(new_state,num):
+                successors.append(new_state)    
     return successors
 
-def bfs(start_state, goal_state):
+def bfs(start_state, goal_state,num):
     queue = deque([(start_state, [])])
     visited = set()
     while queue:
@@ -38,14 +34,15 @@ def bfs(start_state, goal_state):
         path = path + [state]
         if state == goal_state:
             return path
-        for successor in get_successors(state):
+        for successor in get_successors(state,num):
             queue.append((successor, path))
     return None
 
-start_state = (3, 3, 1)
+num=4
+start_state = (num, num, 1)
 goal_state = (0, 0, 0)
 
-solution = bfs(start_state, goal_state)
+solution = bfs(start_state, goal_state,num)
 if solution:
     print("Solution found:")
     for step in solution:
